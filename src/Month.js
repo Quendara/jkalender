9191f8b22d394a2670065031a6958bb8697dec32
Quendara <andregarstka@web.de>
2020-04-06 10:09:56 +0200

    2 files modified, 2 added, 2 deleted

---
+++ src/Month.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Month = ( m ) => { 
+  return ( <img src="img/0.jpg" />)
+}
--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { Month } from "./Month";
+import "./style.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
+const App = () => {
+  return (
+    <div>
+      <p>Start editing to see some magic happen :)</p>
+      <Month />
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
