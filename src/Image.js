7c5ed51236136824f5c878ee18c9495d2033a078
Quendara <andregarstka@web.de>
2020-04-07 10:23:17 +0200

    Updated with designer

--- src/Image.js
+++ src/Image.js
@@ -15,6 +15,6 @@ export const Image = props => {
       <img src={getSrc()} className="mid-height" />
       
     </>
-  );
+  ); 
   // prefix
 };
--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const App = () => {
   const date = new Date();
 
   useState();
-  const [day, setDay] = useState(date.getDay());
+  const [day, setDay] = useState( 2 );
   const [dayMonth, setDayMonth] = useState( leadingZero( date.getDate(),2 ));
-  const [month, setMonth] = useState(date.getMonth());
+  const [month, setMonth] = useState(date.getMonth() + 1 );
   const [year, setYear] = useState(date.getFullYear());
 
   const randCal = () => {
@@ -35,35 +35,41 @@ const App = () => {
     return ret[digit];
   };
 
+// <!--<Image prefix="" value={digit(dayMonth, 0)} />
+//           <Image value={digit(dayMonth, 1)} />-->  
+
   return (
     <div>
       <div onClick={() => randCal()}>
-        <b>Jonna's Kalender</b>
-        <hr />
+        <b>Jonna's Kalender</b><br />
+        <small className="subTitle">Das man auch in Corona zeiten nicht das Datum vergisst ;)</small>
+   
       </div>
       <div className="row">
-        <div className="col-3  mid-height">
-          <Image prefix="d" value={day} />
+        <div className="col-6  mid-height" >
+          
+          <Image prefix="d" value="ays"className="absolute" />
+          <h1 className="absoluteText" >Dienstag </h1>
+          
         </div>
-        <div className="col-5  mid-height">
+        <div className="col-6  mid-height">
           <Image prefix="" value={digit(dayMonth, 0)} />
-          <Image value={digit(dayMonth, 1)} />
+           <Image value={digit(dayMonth, 1)} />
         </div>
-        <div className="col-3  mid-height">
+        
+      </div>
+      <div className="row">
+        <div className="col-6  mid-height">
           <Image prefix="m" value={month} />
         </div>
+        <div className="col-6  "><Image prefix="zeit" value="f" />  </div>
       </div>
       <div className="row">
+        <div className="col-6  mid-height " >
+        <p className="mid-height spruch " >Blumenduft weht um die Nase bald kommt schon er Osterhase. </p>
+        </div>
         <div className="col-6  mid-height">
           <Image prefix="" value="2020" />
-        </div>
-        <div className="col-6  "> <p className="mid-height" ><br /> Corona, wir schaffen das ;) </p></div>
-      </div>
-      <div className="row">
-        <div className="col-6  mid-height bg-success" >
-        </div>
-        <div className="col-6  mid-height">
-          <Image prefix="zeit" value="f" /> 
         </div>
       </div>
     </div>
