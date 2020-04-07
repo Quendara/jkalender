20ce878cffb291960eacf99dc4ef78c80eb72b02
Quendara <andregarstka@web.de>
2020-04-07 10:59:28 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -5,25 +5,32 @@ import { Library } from "./Library";
 import "./style.css";
 
 function getRandomInt(max) {
-  
   return Math.floor(Math.random() * Math.floor(max - 1)) + 2;
 }
 
-let daynames = [ 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag','Samstag', 'Sonntag' ]
+let daynames = [
+  "Montag",
+  "Dienstag",
+  "Mittwoch",
+  "Donnerstag",
+  "Freitag",
+  "Samstag",
+  "Sonntag"
+];
 
 const App = () => {
   const date = new Date();
 
   useState();
-  const [day, setDay] = useState( date.getDay() );
-  const [dayMonth, setDayMonth] = useState( leadingZero( date.getDate(),2 ));
-  const [month, setMonth] = useState(date.getMonth() + 1 );
+  const [day, setDay] = useState(date.getDay());
+  const [dayMonth, setDayMonth] = useState(leadingZero(date.getDate(), 2));
+  const [month, setMonth] = useState(date.getMonth() + 1);
   const [year, setYear] = useState(date.getFullYear());
 
   const randCal = () => {
     setDay(getRandomInt(7));
-    setMonth( (( month + 1 ) % 11 )  );
-    setDayMonth( leadingZero( getRandomInt( 31 ), 2) );
+    setMonth((month + 1) % 11);
+    setDayMonth(leadingZero(getRandomInt(31), 2));
   };
 
   function leadingZero(num, size) {
@@ -37,37 +44,43 @@ const App = () => {
     return ret[digit];
   };
 
-// <!--<Image prefix="" value={digit(dayMonth, 0)} />
-//           <Image value={digit(dayMonth, 1)} />-->  
+  // <!--<Image prefix="" value={digit(dayMonth, 0)} />
+  //           <Image value={digit(dayMonth, 1)} />-->
 
   return (
     <div>
-      <div onClick={() => randCal()}>
-        <div className="title">Jonna's Kalender</div>
-        <div className="subTitle">Das man auch in Corona zeiten nicht das Datum vergisst ;)</div>
+      <div className="row" onClick={() => randCal()}>
+        <div className="col-12">
+          <div className="title">Jonna's Kalender</div>
+          <div className="subTitle">
+            Das man auch in Corona zeiten nicht das Datum vergisst ;)
+            <br/><br/>
+          </div>
+        </div>
       </div>
       <div className="row">
-        <div className="col-6  mid-height" >
-          
-          <Image prefix="d" value="ays"className="absolute" />
-          <h1 className="absoluteText" > { daynames[ day-1 ] } </h1>
-          
+        <div className="col-6  mid-height">
+          <Image prefix="d" value="ays" className="absolute" />
+          <h1 className="absoluteText"> {daynames[day - 1]} </h1>
         </div>
         <div className="col-6  mid-height">
           <Image prefix="" value={digit(dayMonth, 0)} />
-           <Image value={digit(dayMonth, 1)} />
+          <Image value={digit(dayMonth, 1)} />
         </div>
-        
       </div>
       <div className="row">
         <div className="col-6  mid-height">
           <Image prefix="m" value={month} />
         </div>
-        <div className="col-6  "><Image prefix="zeit" value="f" />  </div>
+        <div className="col-6  ">
+          <Image prefix="zeit" value="f" />{" "}
+        </div>
       </div>
       <div className="row">
-        <div className="col-6  mid-height " >
-        <p className="mid-height spruch " >Blumenduft weht um die Nase bald kommt schon er Osterhase. </p>
+        <div className="col-6  mid-height ">
+          <p className="mid-height spruch ">
+            Blumenduft weht um die Nase, bald kommt schon der Osterhase.{" "}
+          </p>
         </div>
         <div className="col-6  mid-height">
           <Image prefix="" value="2020" />
