e6509a15c891053d330b14e7d219dba85ce8a5c9
Quendara <andregarstka@web.de>
2020-08-19 16:33:33 +0200

    1 file modified

--- src/Settings.js
+++ src/Settings.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Settings = {
   baseAwsUrl : "https://jnofjgywxe.execute-api.eu-central-1.amazonaws.com/dev/",
-  baseS3Bucket : "https://s3.eu-central-1.amazonaws.com/quendara-images-thumbs/jonna/"
-
+  baseS3Bucket : "https://s3.eu-central-1.amazonaws.com/quendara.de/jonna/"
 }
 
 export default Settings;
